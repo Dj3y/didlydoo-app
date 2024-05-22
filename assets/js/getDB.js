99f8a6d0bed9f283fe9eee37f4bc5b30df4f450c
Dj3y <dkyamilova@gmail.com>
2024-05-22 15:49:50 +0200

    modifications

--- assets/js/getDB.js
+++ assets/js/getDB.js
@@ -7,6 +7,35 @@ export function getDB(){
         console.log(data);
         // efface les événement précédents
         divEvent.innerHTML = '';
+            for(let i = 1; i <= data.length; i++){
+                console.log("longeur : " + data.length);
+                const sectionEvent = document.createElement('section');
+                const eventName= document.createElement('h3');
+                eventName.textContent =data.name;
+                console.log(data.name);
+                divEvent.appendChild(sectionEvent);
+                sectionEvent.appendChild(eventName);
+    
+                const eventAuthor = document.createElement('h4');
+                eventAuthor.textContent = data.author;
+                sectionEvent.appendChild(eventAuthor);
+    
+                const eventDescription = document.createElement('p');
+                eventDescription.textContent = data.description;
+                sectionEvent.appendChild(eventDescription);
+    
+                // création du tableau 
+                // création des en-têtes 
+                const table = document.createElement('table');
+                const headerRow = document.createElement('tr');
+                const dateHeader = document.createElement('th');
+                dateHeader.textContent = 'Nom / Dates';
+                headerRow.appendChild(dateHeader);
+    
+                sectionEvent.appendChild(table);
+                table.appendChild(headerRow);
+                headerRow.appendChild(dateHeader);
+            }
          
         // //affichage des événements existants
         data.forEach((element) => {
@@ -33,13 +62,12 @@ export function getDB(){
             dateHeader.textContent = 'Nom / Dates';
             headerRow.appendChild(dateHeader);
 
-            divEvent.appendChild(table);
+            sectionEvent.appendChild(table);
             table.appendChild(headerRow);
             headerRow.appendChild(dateHeader);
            
 
             element.dates.forEach((dateInfo) => {
-                let i = 0;
                 const dataCell = document.createElement('td');
                 dataCell.textContent = dateInfo.date;
                 // console.log(dataCell.textContent);
@@ -47,13 +75,16 @@ export function getDB(){
 
                 const attendeesRow = document.createElement('tr');
                 const attendeesCell =document.createElement('td');
-                // const attendeesList = dateInfo.attendees.map(attendee => attendee.name).join(', ');
+                const attendeesList = dateInfo.attendees.map(attendee => attendee.name);
                 // récuperation du nom SOUCI
-                const attendeesList = dateInfo.attendees.name;
+                const attendeesLength = dateInfo.attendees.length;
+                console.log(attendeesLength);
+                // const attendeesList = dateInfo.attendees.name;
                 attendeesRow.textContent = attendeesList;
                 console.log(attendeesRow.textContent);
                 // attendeesCell.appendChild(attendeesList);
                 attendeesRow.appendChild(attendeesCell);
+                table.appendChild(attendeesRow);
 
             //     const availableCell = document.createElement('tr');
             //     const availableList =document.createElement('td');
