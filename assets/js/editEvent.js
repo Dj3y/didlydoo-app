5f3d9d7a155dcc52aef786b0c02763b96e97e64b
Masy Jordan <masyjordan@gmail.com>
2024-05-30 23:05:36 +0200

    add light Mode

--- assets/js/editEvent.js
+++ assets/js/editEvent.js
@@ -4,21 +4,21 @@ getDB();
 export function editEvents(id){
 
     console.log(id);
-    const titleEvent =document.getElementsByClassName('event-title-edit').value;
+    const titleEvent =document.querySelector('event-title-edit').value;
     console.log("TEEEEEEEEEEEEEST = "+titleEvent)
-    const descriptionEvent = document.getElementsByClassName('event-description-edit').value;
+    const descriptionEvent = document.querySelector('event-description-edit').value;
 
-    let titleEdit = document.querySelector("#event-title");
-      titleEdit.textContent='';
+    // let titleEdit = document.querySelector("#event-title");
+    //   titleEdit.textContent='';
 
-      let authorEdit  = document.querySelector("#event-author");
-      authorEdit.textContent='';
+    //   let authorEdit  = document.querySelector("#event-author");
+    //   authorEdit.textContent='';
 
-      let dateEdit  = document.querySelector("#event-date");
-      dateEdit.textContent='';
+    //   let dateEdit  = document.querySelector("#event-date");
+    //   dateEdit.textContent='';
 
-      let descriptionEdit  = document.querySelector("#event-description");
-      descriptionEdit.textContent='';
+    //   let descriptionEdit  = document.querySelector("#event-description");
+    //   descriptionEdit.textContent='';
     const edit =  {
         "name": titleEvent.value,
         "description": descriptionEvent.value
